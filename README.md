2d4ffc85e5c807fad597cd51287193db3a8f89e9
Cole Murphy <cole.murphy29@gmail.com>
2017-02-10 00:45:30 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Setup instructions:
+1. generate a slack api token
+1. add an environmental variable called "SLACK_API_TOKEN" with your token
+1. Create a virtualenv called venv
+1. `source venv/bin/activate`
+1. `pip install -r requirements.txt`
+1. 
---
+++ lunchbot.py
@@ -0,0 +1,38 @@
+from datetime import datetime, time, timedelta 
+import random
+import os
+import requests
+import urllib
+
+def run():
+    lunchtime, leaving_time = get_lunch_time()
+    print(leaving_time.strftime("%I:%M:%S"))
+    token = os.environ["SLACK_API_TOKEN"]
+    #members = get_group_members(token)
+    for member in ("U03A4RWP9"):
+        send_slack_reminder(token, "meet in the lobby in two minutes ({0})"
+            .format(leaving_time.strftime("%I:%M:%S")),"1486711200", "U03A4RWP9")
+
+def get_lunch_time():
+    #timedelta requires a datetime, we don't care about date
+    basetime = datetime(100, 1, 1, 11, 47, 00)
+    r = timedelta(seconds=(random.randint(0, 299))) 
+    lunchtime = (basetime + r).time()
+    leaving_time = (basetime + r - timedelta(seconds=120)).time()
+    return lunchtime, leaving_time
+
+def get_group_members(token, group="G2V24RAV6"):
+    r = requests.get('https://slack.com/api/groups.info?token={0}&channel={1}'
+        .format(token, group))
+    return r.json()["group"]["members"]
+
+def send_slack_reminder(token, text, time, user):
+    payload = {'token':token,'text':text,'time':time,'user':user}
+    r = requests.get('https://slack.com/api/reminders.add', params=payload)
+    print r.url
+    if(r.json()["ok"] != True):
+        print(str(r.json()))
+        raise Exception("the slack reminder failed. please contact your system administrator")
+
+if __name__ == '__main__':
+    run()
