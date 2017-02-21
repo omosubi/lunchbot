66a80e3d5e0d8d23a0794b37374941c26b54312a
Cole Murphy <cmurphy@kcura.com>
2017-02-21 10:24:08 -0600

    refactor a the slack api calls

--- lunchbot.py
+++ lunchbot.py
@@ -2,17 +2,18 @@ from datetime import datetime, time, timedelta
 import random
 import os
 import requests
-import urllib
+
+token = None
+slack_url = 'https://slack.com/api/'
 
 def run():
     lunchtime, leaving_time = get_lunch_time()
     print(leaving_time.strftime("%I:%M:%S"))
-    token = os.environ["SLACK_API_TOKEN"]
-    members = get_group_members(token)
+    members = send_to_slack('groups.info', channel="G2V24RAV6")["group"]["members"]
     for member in members:
-        send_slack_reminder(token, "meet in the lobby in two minutes ({0})"
-            .format(lunchtime.strftime("%I:%M:%S")),"at {0}"
-            .format(leaving_time.strftime("%I:%M:%S"))), member)
+        send_slack_reminder("reminders.add", text="meet in the lobby in two minutes ({0})"
+            .format(lunchtime.strftime("%I:%M:%S")), time="at {0}"
+            .format(leaving_time.strftime("%I:%M:%S"))), user=member)
 
 def get_lunch_time():
     #timedelta requires a datetime, we don't care about date
@@ -22,18 +23,14 @@ def get_lunch_time():
     leaving_time = (basetime + r - timedelta(seconds=120)).time()
     return lunchtime, leaving_time
 
-def get_group_members(token, group="G2V24RAV6"):
-    r = requests.get('https://slack.com/api/groups.info?token={0}&channel={1}'
-        .format(token, group))
-    return r.json()["group"]["members"]
-
-def send_slack_reminder(token, text, time, user):
-    payload = {'token':token,'text':text,'time':time,'user':user}
-    r = requests.get('https://slack.com/api/reminders.add', params=payload)
-    print r.url
+def send_to_slack(method, **payload):
+    payload['token'] = token
+    r = requests.get("{0}{1}".format(slack_url, method), payload)
     if(r.json()["ok"] != True):
         print(str(r.json()))
-        raise Exception("the slack reminder failed. please contact your system administrator")
+        raise Exception("the slack message failed. please contact your system administrator")
+    return r.json()
 
 if __name__ == '__main__':
+    token = os.environ["SLACK_API_TOKEN"]
     run()
